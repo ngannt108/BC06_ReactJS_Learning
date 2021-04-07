2a261b7698867a37d9096551c6bfcac4c5a18fd9
thungan10082000 <ngannguyen10820@gmail.com>
2021-04-07 20:41:43 +0700

    init project

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//bootstrap
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/jquery/dist/jquery.min.js";
+import "../node_modules/popper.js/dist/popper.min.js";
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+
 
 ReactDOM.render(
   <React.StrictMode>
