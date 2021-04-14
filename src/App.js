73161b259134b837c6817b82c83c992efe49251c
thungan10082000 <ngannguyen10820@gmail.com>
2021-04-14 07:50:52 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
 import Carousel from "./components/Carousel";
-import ProductList from "./components/ProductList";
+import Content from "./components/Content";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
-      <div>
-        <Header />
-        <Carousel />
-        <ProductList />
-        <Footer />
-      </div>
+      {/* Bài tập chia Component */}
+      <Header />
+      <Carousel />
+      <Content />
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,41 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-class Footer extends Component{
-    render(){
-        return (
-            <section id="promotion" className="container-fluid pt-5 pb-5 bg-dark">
-            <h1 className="text-center text-white">PROMOTION</h1>
-            <div className="container bg-light pt-5 pb-5">
-              <div className="row">
-                <div className="col-xs-12 col-sm-12 col-md-4">
-                  <div className="container">
-                    <img
-                      src="./img/promotion_1.png"
-                      style={{ maxWidth: "100%" }}
-                    />
-                  </div>
-                </div>
-                <div className="col-xs-12 col-sm-12 col-md-4">
-                  <div className="container">
-                    <img
-                      src="./img/promotion_2.png"
-                      style={{ maxWidth: "100%" }}
-                    />
-                  </div>
-                </div>
-                <div className="col-xs-12 col-sm-12 col-md-4">
-                  <div className="container">
-                    <img
-                      src="./img/promotion_3.jpg"
-                      style={{ maxWidth: "100%" }}
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </section>
-      
-        );
-    }
+class Footer extends Component {
+  render() {
+    return (
+      <div
+        className="footer"
+        style={{
+          backgroundColor: "green",
+          textAlign: "center",
+          padding: "15px 0",
+          color: "white",
+        }}
+      >
+        Copyright by CyberSoft
+      </div>
+    );
+  }
 }
-export default Footer
+
+export default Footer;
